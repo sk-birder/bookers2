320e0671f0b977cc3e19c2ab537caeddd7ed28aa
sk-birder <199427351+sk-birder@users.noreply.github.com>
2025-04-08 01:34:44 +0000

    [Edit] ヘッダー部にリンクを追加(サインアウト除く)

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:new, :create, :index, :show, :update, :edit]
   resources :books
-  get 'homes/about'
+  get 'homes/about', as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
 
